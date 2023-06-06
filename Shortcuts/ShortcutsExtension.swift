f516bd3c50775a71f698a0c8dbd92bc8c819246b
Javier García <develop@javier.games>
2023-06-06 12:19:49 +0900

    added: AppIntent for shortcuts.

---
+++ Shortcuts/ShortcutsExtension.swift
@@ -0,0 +1,12 @@
+//
+//  ShortcutsExtension.swift
+//  Shortcuts
+//
+//  Created by Javier García on 2023/05/08.
+//
+
+import AppIntents
+
+@main
+struct ShortcutsExtension: AppIntentsExtension {
+}
